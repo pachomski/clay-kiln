eb0f99d58acf57b08abbeeb684e03c29d43e631c
Christopher Pachomski <christopher.pachomski@turner.com>
2019-08-31 22:12:43 -0400

    create storybook-toolbar and inject into page with storybook.js

---
+++ lib/toolbar/storybook-toolbar.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="kiln-wrapper storybook-mode">
+		<!-- <alert-container></alert-container>
+		<div class="kiln-progress">
+			<progress-bar></progress-bar>
+		</div> -->
+		<ui-toolbar Title="Storybook" :raised="false">
+			 <ui-icon-button
+				color="black"
+				icon="more_vert"
+				size="large"
+				type="secondary"
+			></ui-icon-button>
+		</ui-toolbar>
+	</div>
+</template>
+
+<script>
+import UiToolbar from 'keen/UiToolbar';
+import UiMenu from 'keen/UiMenu';
+import UiIconButton from 'keen/UiIconButton';
+import alertContainer from './alert-container.vue';
+import progressBar from './progress.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    'ui-toolbar': UiToolbar,
+    'alert-container': alertContainer,
+    'progress-bar': progressBar,
+    'ui-icon-button': UiIconButton
+
+  }
+};
+</script>
+
+<style lang="sass">
+   	@import '../../styleguide/toolbar';
+	@import '../../styleguide/layers';
+
+	.kiln-wrapper {
+    	@include toolbar-wrapper();
+	}
+</style>
--- storybook.js
+++ storybook.js
@@ -1,1 +1,58 @@
-alert('test')
+import Vue from 'vue';
+import store from './lib/core-data/store';
+import utilsAPI from './lib/utils/api';
+import VueNProgress from 'vue-nprogress';
+import conditionalFocus from './directives/conditional-focus';
+import storybookToolbar from './lib/toolbar/storybook-toolbar.vue';
+import VueClickOutside from 'vue-click-outside';
+import 'keen-ui/src/bootstrap'; // import this once, for KeenUI components
+
+// export api for plugins, validators, inputs, buttons, etc
+window.kiln = window.kiln || {};
+
+// .plugins, .inputs, .validators, and .panes objects should already exist
+window.kiln.utils = utilsAPI;
+
+const nprogress = new VueNProgress({
+  parent: '.nprogress-container',
+  template: '<div class="bar" role="bar"></div>',
+  showSpinner: false,
+  easing: 'linear',
+  speed: 500,
+  trickle: false,
+  minimum: 0.001
+});
+
+// add progress bar
+Vue.use(VueNProgress, {
+  router: false,
+  http: false
+});
+
+Vue.directive('conditional-focus', conditionalFocus());
+Vue.directive('click-outside', VueClickOutside);
+
+// Require all scss/css files needed
+require.context('./styleguide', true, /^.*\.(scss|css)$/);
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    debug: process.env.NODE_ENV !== 'production',
+    strict: true,
+    el: '#kiln-app',
+    render: h => h('storybook-toolbar'),
+    store,
+    nprogress,
+    components: {
+      'storybook-toolbar': storybookToolbar
+    }
+  });
+
+  // page load indicator. will be finished by the preloader
+  store.dispatch('startProgress', 'offline');
+
+  // initialize kiln plugins from app with store
+  if (window.kiln.plugins && Array.isArray(window.kiln.plugins)) {
+    window.kiln.plugins.forEach(plugin => plugin(store));
+  }
+});
